48180f5fbc906c120ad2d18dd386c4948bcdb7a5
Chase King <chasechocolate@comcast.net>
2017-04-09 10:47:24 -0700

    Initial push. Rough website layout - no navigation, not much styling, etc.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$.getJSON("./json/schedule.json", function(json){
+    var table = $("#scheduletable");
+    var days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
+    //Table header
+    var html = "<thead><tr>";
+
+    html += "<td></td>";
+
+    for(i in days){
+        html += "<th>" + days[i] + "</th>";
+    }
+
+    html += "</tr></thead>";
+    table.append(html);
+
+    //Table body
+    html = "<tbody>";
+
+    function getTimeLabel(time, color){
+        return "<span class='tabledata" + (color ? " " + color : "") + "'>" + time + "</span>";
+    }
+
+    for(className in json){
+        var classData = json[className];
+        html += "<tr>";
+        html += "<th scope='row'>" + className + "</th>";
+
+        for(i in days){
+            var day = days[i];
+            html += "<td>";
+
+            if(classData[day]){
+                if(classData[day] instanceof Array){
+                    for(i in classData[day]){
+                        html += getTimeLabel(classData[day][i], classData.color);
+                    }
+                } else {
+                    html += getTimeLabel(classData[day], classData.color);
+                }
+            }
+
+            html += "</td>";
+        }
+
+        html += "</tr>";
+    }
+
+    html += "</tbody>";
+    table.append(html);
+});
